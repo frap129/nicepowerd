b4e3ee5dd3209d077b47431257845670b1cb8a03
Joe Maples <joe@maples.dev>
2022-12-13 11:04:43 -0500

    PKGBUILD: Use local source

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # AC_PATH changes between devices. Replace the value with the correct
 # /sys/class/power_supply/<some directory>/online for your system.
-AC_PATH="/sys/class/power_supply/ADP0/online"
+AC_PATH="/sys/class/power_supply/AC/online"
 
 
